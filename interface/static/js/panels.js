01300588bc5e9ed0ea838b0154489a84c694a492
Dirk Koelewijn <d.koelewijn@student.utwente.nl>
2018-03-05 16:04:36 +0000

    Merged in feature-dk-general-interface (pull request #2)
    Feature dk general interface
    
    * Added BootStrap 4.0.0 to static pages
    
    * Added jQuery 3.3.1 to static pages
    
    * Created basic interface with basic panels
    
    * Added panel switching to interface
    
    * Code Cleanup
    
    * Added minor missing documentation
    
    Approved-by: VincentDun <v.a.dunning@student.utwente.nl>

---
+++ interface/static/js/panels.js
@@ -0,0 +1,56 @@
+/*
+JS for panels
+
+Created by Dirk Koelewijn <d.koelewijn@student.utwente.nl> for the
+RoboTeam Twente as part of the Design Project for the Bachelor
+Technical Computer Science of the University of Twente.
+
+All Rights Reserved.
+*/
+
+
+// Bind hide and switch buttons
+$(".panel-hide, .panel-switch").click(function (event) {
+  showOtherPanel($(event.target).closest(".panel"));
+});
+
+// Bind full screen buttons
+$(".panel-full-screen").click(function (event) {
+  showSinglePanel($(event.target).closest(".panel"));
+});
+
+// Bind show other buttons
+$(".panel-show-other").click(function () {
+  showAllPanels();
+});
+
+/**
+ * Function to show all panels
+ */
+function showAllPanels() {
+  $(".panel-full-screen, .panel-hide").css("display", "inline");
+  $(".panel").show();
+  $(".panel-show-other, .panel-switch").hide();
+}
+
+/**
+ * Function to show a single panel
+ *
+ * @param panel The panel to show
+ */
+function showSinglePanel(panel) {
+  $(".panel, .panel-full-screen, .panel-hide").hide();
+  $(panel).show();
+  $(".panel-show-other, .panel-switch").css("display", "inline");
+}
+
+/**
+ * Function to show the other panel instead of this one
+ * @param thisPanel The current pannel
+ */
+function showOtherPanel(thisPanel) {
+  $(".panel").show();
+  $(".panel-full-screen, .panel-hide").hide();
+  $(thisPanel).hide();
+  $(".panel-show-other, .panel-switch").css("display", "inline");
+}
