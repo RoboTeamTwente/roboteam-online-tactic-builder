e31e2b90d00f847011a7decb1210e54721b40556
Sander Meinderts <sander_meinderts@hotmail.com>
2018-03-12 10:06:54 +0100

    Implemented edit tree function, checked with serializers

--- websimulator/connections/serializers.py
+++ websimulator/connections/serializers.py
@@ -5,12 +5,40 @@ deligate all the action to the appropriate Serializers to let them handle
 their part.
 """
 
-from rest_framework.serializers import Serializer, ChoiceField, DictField
+from rest_framework.serializers import Serializer, ChoiceField, DictField, CharField, ListField
 
+
+class TreeSerializer(Serializer):
+    """
+    The TreeSerializer is used to validate incoming JSON objects against the
+    minimal requirements of a tree.
+    """
+
+    root = CharField()
+    nodes = DictField()
+
+    def validate_title(self, data):
+        data["title"] = "root"
+        return data
+
+
+class CustomNodesSerializer(Serializer):
+    """
+    The CustomNodesSerializer is used to validate the JSON object against the
+    minimal requirements of a custom nodes section.
+    """
+
+    custom_nodes = ListField()
+
+class TreeRootSerializer(Serializer):
+    """
+
+    """
+    tree = DictField()
 
 class ProtocolSerializer(Serializer):
     """
-    The ProtocolSerializer is used to validate incoming JSON object agains the
+    The ProtocolSerializer is used to validate incoming JSON object against the
     protocol used for communication over the websocket.
     """
 
@@ -27,9 +55,19 @@ class ProtocolSerializer(Serializer):
         Validator for the SIM action.
         :param values: The values passed by the client with the action.
         """
-        serializer = StartSimulatorSerializer(data=values)
-        serializer.is_valid(raise_exception=True)
-        return values
+        tr_serializer = TreeRootSerializer(data=values)
+        tr_serializer.is_valid(raise_exception=True)
+        values = tr_serializer.validated_data["tree"]
+
+        tree_serializer = TreeSerializer(data=values)
+        cn_serializer = CustomNodesSerializer(data=values)
+        tree_serializer.is_valid(raise_exception=True)
+        cn_serializer.is_valid(raise_exception=True)
+
+        return {
+            "tree": tree_serializer.validated_data,
+            "custom_nodes": cn_serializer.validated_data["custom_nodes"]
+        }
 
     def stop_sim(self, values):
         """
@@ -65,15 +103,3 @@ class ProtocolSerializer(Serializer):
         data["values"] = getattr(self, method)(data["values"])
 
         return data
-
-
-class StartSimulatorSerializer(Serializer):
-    """
-    The StartSimulatorSerializer is used to validate the values given with a SIM
-    protocol message.
-    """
-
-    # For now only the behavior3 is needed to start the simulator, later a
-    # 'level' field might be added to indicate which setup should be used in
-    # the simulator.
-    tree = DictField()
