76e1d311c23d9d3eb3ed6658c992ce05f93acd07
VincentDun <v.a.dunning@student.utwente.nl>
2018-03-12 09:12:07 +0000

    Merged in revert-pr-4 (pull request #5)
    Revert "Added Behavior3 editor to interface (pull request #4)"
    
    This reverts pull request #4.
    
    > * Added minor missing documentation
    >
    > * put the b3js editor in the general interface framework
    >
    > * Fix: Panel resize/fullscreen/hide etc.
    >
    > * Removed top bar from editor
    >
    > * Fix: Panel resize/fullscreen/hide etc. (attempt 2)
    >
    > * added functionality to the run simulator button
    >
    > * Cleaned up code
    >
    > * Updated documentation

--- interface/static/js/panels.js
+++ interface/static/js/panels.js
@@ -1,27 +1,27 @@
 /*
 JS for panels
 
-Created for the RoboTeam Twente as part of the Design Project for the Bachelor
+Created by Dirk Koelewijn <d.koelewijn@student.utwente.nl> for the
+RoboTeam Twente as part of the Design Project for the Bachelor
 Technical Computer Science of the University of Twente.
 
 All Rights Reserved.
 */
 
-$.ready(function () {
-  // Bind hide and switch buttons
-  $(".panel-hide, .panel-switch").click(function (event) {
-    showOtherPanel($(event.target).closest(".panel"));
-  });
 
-  // Bind full screen buttons
-  $(".panel-full-screen").click(function (event) {
-    showSinglePanel($(event.target).closest(".panel"));
-  });
+// Bind hide and switch buttons
+$(".panel-hide, .panel-switch").click(function (event) {
+  showOtherPanel($(event.target).closest(".panel"));
+});
 
-  // Bind show other buttons
-  $(".panel-show-other").click(function () {
-    showAllPanels();
-  });
+// Bind full screen buttons
+$(".panel-full-screen").click(function (event) {
+  showSinglePanel($(event.target).closest(".panel"));
+});
+
+// Bind show other buttons
+$(".panel-show-other").click(function () {
+  showAllPanels();
 });
 
 /**
--- websimulator/pages/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.urls import path
-from .views import *
-
-app_name = 'pages'
-urlpatterns = [
-    path('', IndexView.as_view(), name='index'),
-    path('editor/', EditorView.as_view(), name='editor')
-]
