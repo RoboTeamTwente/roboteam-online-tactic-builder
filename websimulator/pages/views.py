cc945b6b0a97877315f43085ffcf0e45ca9d47ba
Sander Meinderts <sander_meinderts@hotmail.com>
2018-03-20 15:00:22 +0100

    New view for trees

--- websimulator/pages/views.py
+++ websimulator/pages/views.py
@@ -1,7 +1,10 @@
-from django.http import HttpRequest
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from django.views import View
+import json
 
+from .models import Tree, add_new_tree, Account
+from social_django.models import UserSocialAuth
 
 # Create your views here.
 
@@ -18,3 +21,17 @@ class EditorView(View):
 
     def get(self, request: HttpRequest):
         return render(request, "b3js/editor.html", {})
+
+
+class TreeView(View):
+    http_method_names = ['get','post']
+
+    def post(self, request: HttpRequest):
+        tree = json.loads(request.body.decode("utf-8"))
+        if UserSocialAuth.objects.filter(user=request.user).exists():
+            add_new_tree(tree, Account.objects.get(
+                user=UserSocialAuth.objects.get(
+                user=request.user)))
+
+        return HttpResponse(200)
+
