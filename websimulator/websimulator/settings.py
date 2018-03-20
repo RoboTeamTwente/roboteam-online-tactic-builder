f8fa6ebebf163ebc65b0f9bfc6705ee519d0e2eb
Sander Meinderts <sander_meinderts@hotmail.com>
2018-03-20 15:00:11 +0100

    Adjusted pipeline for social authentication

--- websimulator/websimulator/settings.py
+++ websimulator/websimulator/settings.py
@@ -157,6 +157,15 @@ LOGIN_URL = 'pages:index'
 LOGIN_REDIRECT_URL = 'pages:index'
 LOGOUT_REDIRECT_URL = 'pages:index'
 
-# SOCIAL_AUTH_PIPELINE = (
-# 'social_core.pipeline.user.user_details',
-# )
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+    'pages.models.add_user_model'
+)
