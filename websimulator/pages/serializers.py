d70ac41d5f95b71f8fcbef0d4c77e6b681e3ea00
Vincent <v.a.dunning@student.utwente.nl>
2018-03-20 15:14:32 +0100

    added API

--- websimulator/pages/serializers.py
+++ websimulator/pages/serializers.py
@@ -1,7 +1,7 @@
-from websimulator.pages.models import custom_node
+from .models import custom_node
 from rest_framework import serializers
 
-class custom_node_serializer(serializers.HyperlinkedModelSerializer):
+class CustomNodeSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = custom_node
         fields = ('name', 'type')
--- websimulator/pages/views.py
+++ websimulator/pages/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpRequest
 from django.shortcuts import render
 from django.views import View
+from .models import custom_node
+from rest_framework import viewsets
+from .serializers import CustomNodeSerializer
 
 
 # Create your views here.
@@ -18,3 +21,8 @@ class EditorView(View):
 
     def get(self, request: HttpRequest):
         return render(request, "b3js/editor.html", {})
+
+
+class CustomNodeViewSet(viewsets.ModelViewSet):
+    queryset = custom_node.objects.all()
+    serializer_class = CustomNodeSerializer
