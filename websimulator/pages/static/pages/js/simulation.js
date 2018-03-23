dca19ab1fe22faa7f7ab1eb2b1e3e44bda6d0a0b
Jeroen Bos <jeroenbosleusden@gmail.com>
2018-03-23 13:52:28 +0100

    Added status updates to the front end simulators

--- websimulator/pages/static/pages/js/simulation.js
+++ websimulator/pages/static/pages/js/simulation.js
@@ -10,24 +10,100 @@ All Rights Reserved.
 
 var queue = [];
 
-function runSimulation() {
-  var editor = document.getElementById("b3js-editor").contentWindow.app.view;
-  var myJSON = {
-    "action": "SIM",
-    "values": {"tree": JSON.parse(editor.exportToJSON())}
-  };
-
-  var ws = new WebSocket("ws://localhost:8000/");
-  ws.onmessage = function (evt) {
-    data = JSON.parse(evt.data);
-    for(i = 0; i < data.body.simulator_output.length; i++){
-      queue.push(data.body.simulator_output[i])
+var AnimationStatus = {
+    READY: 1,
+    SUBMITTED: 2,
+    QUEUED: 3,
+    GENERATING: 4,
+    STARTING_SIMULATION: 5,
+    SIMULATING: 6,
+    BUFFERING: 7,
+    FINISHED: 8,
+    properties: {
+        1: {
+            action: function (show, hide) {
+                show("Ready to start a new simulation.", "primary");
+            },
+        },
+        2: {
+            action: function (show, hide) {
+                show("Submitted tree to the server.", "secondary");
+            }
+        },
+        3: {
+            action: function(show, hide) {
+                show("Placed into simulation queue.", "secondary");
+            }
+        },
+        4: {
+            action: function(show, hide) {
+                show("Generating the code for simulation, this will take about 30 seconds.", "warning");
+            }
+        },
+        5: {
+            action: function(show, hide) {
+                show("Starting the simulation, it will start in a few seconds.", "success");
+            }
+        },
+        6: {
+            action: function(show, hide) {
+                hide();
+            }
+        },
+        7: {
+            action: function(show, hide) {
+                show("Buffering the simulation results.", "info");
+            }
+        },
+        8: {
+            action: function(show, hide) {
+                show("The simulation has finished.", "primary");
+            }
+        }
     }
-  };
-  ws.onclose = function (event) {
-    console.log(event)
-  };
-  ws.onopen = function () {
-    ws.send(JSON.stringify(myJSON));
-  };
+};
+
+var gui_status = AnimationStatus.READY;
+var buffer_size = 30;
+
+function changeGuiStatus(status, simulator) {
+    gui_status = status;
+    AnimationStatus.properties[gui_status].action(simulator.showOverlay, simulator.hideOverlay);
+}
+
+function runSimulation() {
+    var editor = document.getElementById("b3js-editor").contentWindow.app.view;
+    var myJSON = {
+        "action": "SIM",
+        "values": {"tree": JSON.parse(editor.exportToJSON())}
+    };
+
+    var ws = new WebSocket("ws://localhost:8000/");
+    ws.onmessage = function (evt) {
+        var data = JSON.parse(evt.data);
+        console.log(data);
+
+        if ("simulator_output" in data.body) {
+            for (i = 0; i < data.body.simulator_output.length; i++) {
+                queue.push(data.body.simulator_output[i])
+            }
+        }
+
+        if ("simulator_status" in data.body) {
+            if (data.body.simulator_status === AnimationStatus.FINISHED) {
+                queue.push({
+                    frame_number: -1
+                });
+                changeGuiStatus(AnimationStatus.SIMULATING, getSimulator());
+            } else {
+                changeGuiStatus(data.body.simulator_status, getSimulator());
+            }
+        }
+    };
+    ws.onclose = function (event) {
+        console.log(event)
+    };
+    ws.onopen = function () {
+        ws.send(JSON.stringify(myJSON));
+    };
 }
